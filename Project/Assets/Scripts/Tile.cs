be56159f65c1e6744d3d52184c980488155d828b
Thaid <david.thai22@gmail.com>
2014-10-04 17:32:06 -0600

    Updated git ignore file

---
+++ Project/Assets/Scripts/Tile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+	
+	public Vector2 gridPosition = Vector2.zero;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseEnter() {
+		//When hovering over tile it changes the color
+		transform.renderer.material.color = Color.green;
+
+		//Use for debugging
+		Debug.Log("Mouse position is (" + gridPosition.x + "," + gridPosition.y +")");
+	}
+	
+	void OnMouseExit() {
+		//When NOT hovering over tile it changes the color
+		transform.renderer.material.color = Color.white;
+	}
+	
+	
+	void OnMouseDown() {
+		GameManager.instance.MovePlayer(this);
+	}
+	
+}
