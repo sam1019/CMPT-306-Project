413e408cc40649bc4458b4d9b970e74ae2b486ff
Henry0422 <ronglihan@uofs-10-227-138-108.usask.ca>
2014-11-25 16:29:49 -0600

    Delete Display HP GUI in Helicopter & Specialist

--- Project/Assets/Scripts/HumanUnitScripts/Helicopter.cs
+++ Project/Assets/Scripts/HumanUnitScripts/Helicopter.cs
@@ -205,11 +205,5 @@ public class Helicopter : Player {
 		}
 		base.TurnOnGUI ();
 	}
-	
-	//Display HP
-	public void OnGUI(){
 
-		Vector3 location = Camera.main.WorldToScreenPoint (transform.position)+ Vector3.up*30+ Vector3.left*15;
-		GUI.TextArea(new Rect(location.x, Screen.height - location.y, 30, 20), HP.ToString());
-	}
 }
