6a12ba4ea551d820ce8ed3eca7605f54956dbf4d
66813185 <Doris@MacBook-Pro.local>
2014-11-24 23:31:16 -0600

    add victory and defeat scenes

---
+++ Project/Assets/Scripts/Scenes/Defeat.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defeat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		
+		float widthScale = 0.2f;
+		float heightScale = 0.05f;
+
+		if (GUI.Button (new Rect (Screen.width * (1 - widthScale)/2, Screen.height * (1 - heightScale + 0.45f)/2, Screen.width * widthScale, Screen.height * heightScale), "Try Again!")) 
+		{
+			Application.LoadLevel ("project");
+		}
+
+		if (GUI.Button (new Rect (Screen.width * (1 - widthScale)/2, Screen.height * (1 - heightScale + 0.60f)/2, Screen.width * widthScale, Screen.height * heightScale), "Go Back")) 
+		{
+			Application.LoadLevel ("LevelSelectScene");
+		}
+		
+	}
+}
---
+++ Project/Assets/Scripts/Scenes/Victory.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Victory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		
+		float widthScale = 0.2f;
+		float heightScale = 0.05f;
+
+//		if(LevelSelect.levelMap = "Map_Level1"){
+//			if (GUI.Button (new Rect (Screen.width * (1 - widthScale)/2, Screen.height * (1 - heightScale - 0.15f)/2, Screen.width * widthScale, Screen.height * heightScale), "Continue: Level 2")) 
+//			{
+//				LevelSelect.levelMap = "Map_Level2";
+//				LevelSelect.levelUnits = "Units_Level2";
+//				Application.LoadLevel ("project");
+//			}
+//		}
+//		if(LevelSelect.levelMap = "Map_Level2"){
+//			if (GUI.Button (new Rect (Screen.width * (1 - widthScale)/2, Screen.height * (1 - heightScale - 0.15f)/2, Screen.width * widthScale, Screen.height * heightScale), "Continue: Level 3")) 
+//			{
+//				LevelSelect.levelMap = "Map_Level3";
+//				LevelSelect.levelUnits = "Units_Level3";
+//				Application.LoadLevel ("project");
+//			}
+//		}
+//		if(LevelSelect.levelMap = "Map_Level3"){
+//			if (GUI.Button (new Rect (Screen.width * (1 - widthScale)/2, Screen.height * (1 - heightScale - 0.15f)/2, Screen.width * widthScale, Screen.height * heightScale), "Continue: Level 4")) 
+//			{
+//				LevelSelect.levelMap = "Map_Level4";
+//				LevelSelect.levelUnits = "Units_Level4";
+//				Application.LoadLevel ("project");
+//			}
+//		}
+//		if(LevelSelect.levelMap = "Map_Level4"){
+//			if (GUI.Button (new Rect (Screen.width * (1 - widthScale)/2, Screen.height * (1 - heightScale - 0.15f)/2, Screen.width * widthScale, Screen.height * heightScale), "Continue: Level 5")) 
+//			{
+//				LevelSelect.levelMap = "Map_Level5";
+//				LevelSelect.levelUnits = "Units_Level5";
+//				Application.LoadLevel ("project");
+//			}
+//		}
+		if (GUI.Button (new Rect (Screen.width * (1 - widthScale)/2, Screen.height * (1 - heightScale + 0.60f)/2, Screen.width * widthScale, Screen.height * heightScale), "Go Back")) 
+		{
+			Application.LoadLevel ("LevelSelectScene");
+		}
+
+	}
+}
